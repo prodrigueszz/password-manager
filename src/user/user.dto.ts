9e643001b3b7b6c0e28f8356437c62ac015b44a6
prodrigueszz <tiago.paivarodrigues1@gmail.com>
2025-11-06 16:08:51 -0300

    feat: creates dtos

--- src/user/user.dto.ts
+++ src/user/user.dto.ts
@@ -1,9 +1,48 @@
-export type CreateUserInputDTO = {
-  name: string,
-  email: string,
-  password: string
-}
+import { z } from "zod";
 
-export type CreateUserOutputDTO = {
-  id: string
-}
+export const userSchema = z.object({
+  id: z.uuid(),
+  name: z.string().min(3).max(15),
+  email: z.email(),
+  password: z.hash("sha256"),
+});
+
+export const createUserSchema = z.object({
+  name: z.string().min(3).max(15),
+  email: z.email(),
+  password: z.string().min(8).max(16),
+});
+
+export const getUserSchema = z.object({
+  params: z.object({
+    id: z.uuid(),
+  }),
+});
+
+export const updateUserFields = z.object({
+  name: z.string().min(3).max(15),
+  email: z.email(),
+  password: z.hash("sha256"),
+});
+
+export const updateUserSchema = z.object({
+  body: updateUserFields.partial(),
+  params: z.object({
+    id: z.uuid(),
+  }),
+});
+
+export const deleteUserSchema = z.object({
+  id: z.uuid(),
+});
+
+export type CreateUserInputDTO = z.infer<typeof createUserSchema>;
+export type CreateUserOutputDTO = z.infer<typeof userSchema>["id"];
+export type GetUserInputDTO = z.infer<typeof getUserSchema>["params"]["id"];
+export type GetUserOutputDTO = Omit<z.infer<typeof userSchema>, "id">;
+export type DeleteUserDTO = Omit<z.infer<typeof userSchema>, "password">;
+export type UpdateUserDTO = {
+  name?: string;
+  email?: string;
+  password?: string;
+};
